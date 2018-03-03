c47416895f6ff58c83ddce26a341fe636e40cfa3
andreas <andreas@andreas-koch.dk>
2018-03-03 09:33:53 +0100

    serve camera

--- command/server.py
+++ command/server.py
@@ -1,8 +1,19 @@
 from flask import Flask
 import time
+from picamera import PiCamera
+
 
 app = Flask(__name__)
 
 @app.route('/currenttime')
 def current_time():
     return str(time.time())
+
+
+if __name__=="__main__":
+
+    camera = PiCamera()
+    camera.start_preview()
+    time.sleep(1)
+    camera.resolution = (320, 240)
+    camera.framerate = 30
