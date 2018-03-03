2dc1a0c347cc6dda842fcf94b2eb03b6d0726e86
andreas <andreas@andreas-koch.dk>
2018-03-03 09:57:43 +0100

    serve camera

--- command/server.py
+++ command/server.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask import send_file
 import time
 from picamera import PiCamera
+import io
 
 
 app = Flask(__name__)
@@ -8,6 +10,18 @@ app = Flask(__name__)
 @app.route('/currenttime')
 def current_time():
     return str(time.time())
+
+@app.route('/currentimage')
+def current_image():
+    pid="img"
+    image_stream = io.BytesIO()
+    camera.capture(image_stream, 'jpeg')
+    return send_file(
+        image_stream,
+        mimetype='image/jpeg',
+        as_attachment=True,
+        attachment_filename='%s.jpg' % pid)
+
 
 
 if __name__=="__main__":
